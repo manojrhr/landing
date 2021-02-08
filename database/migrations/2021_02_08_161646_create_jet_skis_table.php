24a5629dbddf0e99e3c228ee9a4a3c95c0531247
Manoj Thakur <manojrhr@hotmail.com>
2021-02-08 22:23:48 +0530

    create migration for skijet table

---
+++ database/migrations/2021_02_08_161646_create_jet_skis_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJetSkisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jet_skis', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->tinyInteger('captain');
+            $table->Integer('capacity');
+            $table->text('images');
+            $table->decimal('lat', 10, 7);
+            $table->decimal('long', 10, 7);
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jet_skis');
+    }
+}
