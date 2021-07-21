0bb7a1cf9425d0f7d8c59fd73a466b57a31f00b6
Manoj Thakur <manojrhr@hotmail.com>
2021-07-21 21:30:15 +0530

    Fixed old design for profile

--- resources/views/layouts/web/web.blade.php
+++ resources/views/layouts/web/web.blade.php
@@ -9,18 +9,18 @@
 	<meta name="googlebot" content="noindex">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Dancing+Script:400,700|Muli:300,400" rel="stylesheet">
-	<link rel="stylesheet" href="{{ asset('assets/web/fonts/icomoon/style.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/jquery-ui.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/owl.carousel.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/owl.theme.default.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/owl.theme.default.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/jquery.fancybox.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/bootstrap-datepicker.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/fonts/flaticon/font/flaticon.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/aos.css') }}">
-	<link href="{{ asset('assets/web/css/jquery.mb.ytplayer.min.css') }}" media="all" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="{{ asset('assets/web/css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/fonts/icomoon/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/jquery-ui.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/owl.theme.default.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/owl.theme.default.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/jquery.fancybox.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/bootstrap-datepicker.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/fonts/flaticon/font/flaticon.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/aos.css') }}">
+	<link href="{{ asset('assets/web_old/css/jquery.mb.ytplayer.min.css') }}" media="all" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="{{ asset('assets/web_old/css/style.css') }}">
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
@@ -50,7 +50,7 @@
 					<div class="col-6 col-lg-3">
 						<a class="logo" href="{{ route('home') }}">
 							<h1>Amaze</h1>
-							<!-- <img src="{{ asset('assets/web/images/logo.png') }}" alt="Image" class="img-fluid"> -->
+							<!-- <img src="{{ asset('assets/web_old/images/logo.png') }}" alt="Image" class="img-fluid"> -->
 						</a>
 					</div>
 					<div class="col-lg-9 d-none d-lg-block">
@@ -89,7 +89,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-3 footer-content">
-						<!-- <p class="mb-4"><img src=" {{ asset('assets/web/images/logo.png') }}" alt="Image" class="img-fluid"></p> -->
+						<!-- <p class="mb-4"><img src=" {{ asset('assets/web_old/images/logo.png') }}" alt="Image" class="img-fluid"></p> -->
 						<h1>Amaze</h1>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo minima qui dolor, iusto iure.</p>
 						<!-- <p><a href="#">Learn More</a></p> -->
@@ -127,21 +127,21 @@
 		</div>
 	</div>
 	<div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" /><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#ff5e15" /></svg></div>
-	<script src="{{ asset('assets/web/js/jquery-3.3.1.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery-migrate-3.0.1.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery-ui.js') }}"></script>
-	<script src="{{ asset('assets/web/js/popper.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/bootstrap.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/owl.carousel.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery.stellar.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery.countdown.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/bootstrap-datepicker.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery.easing.1.3.js') }}"></script>
-	<script src="{{ asset('assets/web/js/aos.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery.fancybox.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery.sticky.js') }}"></script>
-	<script src="{{ asset('assets/web/js/jquery.mb.ytplayer.min.js') }}"></script>
-	<script src="{{ asset('assets/web/js/main.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery-3.3.1.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery-migrate-3.0.1.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery-ui.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/popper.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/owl.carousel.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery.stellar.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery.countdown.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/bootstrap-datepicker.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery.easing.1.3.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/aos.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery.fancybox.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery.sticky.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/jquery.mb.ytplayer.min.js') }}"></script>
+	<script src="{{ asset('assets/web_old/js/main.js') }}"></script>
 	<!-- <script async src="gtag/js_id_ua-23581568-13.js"></script> -->
 	<!-- Alertify -->
 	<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
