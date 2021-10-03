94adeebef700cd97efce46273ee25176319aae61
Manoj Thakur <manojrhr@hotmail.com>
2021-10-03 22:10:12 +0530

    Added error page design

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.web.master')
+
+@section('title', __('Not Found'))
+
+@section('styles')
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .code {
+                border-right: 2px solid;
+                font-size: 26px;
+                padding: 0 15px 0 15px;
+                text-align: center;
+            }
+
+            .message {
+                font-size: 18px;
+                text-align: center;
+            }
+        </style>
+@endsection
+
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="code">
+            404
+        </div>
+
+        <div class="message" style="padding: 10px;">
+            Sorry we couldn't find that page!
+        </div>
+        <div class="message" style="padding: 10px;">
+            <h3><a href="{{ route('home') }}">Go to Homepage</a></h3>
+        </div>
+    </div>
+@endsection
