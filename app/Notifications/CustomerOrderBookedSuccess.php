59b1d4ce4ae1a6a6ade18af563c00dce770d7655
Manoj Thakur <manojrhr@hotmail.com>
2021-12-26 15:48:11 +0530

    Admin Notification error fixed

---
+++ app/Notifications/CustomerOrderBookedSuccess.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Booking;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CustomerOrderBookedSuccess extends Notification
+{
+    use Queueable;
+
+    public $booking;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Booking $booking)
+    {
+        $this->booking = $booking;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                ->subject('Your booking request successfully received')
+                ->greeting('Hello! '.$this->booking->user->name)
+                ->line('Thanks for your booking request is successfully received to us. Our team will be in touch with you soon.')
+                ->action('See Booking Details', url('/user/bookings'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
