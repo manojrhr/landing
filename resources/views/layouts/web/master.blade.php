9ef72efdd018cce525dd1328d96fb9222cf00196
Manoj Thakur <manojrhr@hotmail.com>
2022-01-16 22:40:07 +0530

    Home Meta title added

--- resources/views/layouts/web/master.blade.php
+++ resources/views/layouts/web/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="title" content="{!! get_component('home_meta_title') !!}">
     <meta name="keywords" content="@yield('keywords','')">
     <meta name="description" content="@yield('description','')">
     <meta name="author" content="">
