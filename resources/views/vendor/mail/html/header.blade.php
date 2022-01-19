7ddfa4eb589c52618e38da2a2d799caa66c76669
Manoj Thakur <manojrhr@hotmail.com>
2022-01-19 22:51:08 +0530

    Added Kiuki logo to email templates

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ url(get_component('main_logo')) }}" alt="logo" />
+        </a>
+    </td>
+</tr>
